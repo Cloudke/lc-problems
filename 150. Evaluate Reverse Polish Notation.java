0b27b3d303e1f1d99deb8f4155add4a9a587510a
Jiewen Ying <37558033+Cloudke@users.noreply.github.com>
2021-02-24 11:54:26 -0500

    Update 150. Evaluate Reverse Polish Notation.java

--- 150. Evaluate Reverse Polish Notation.java
+++ 150. Evaluate Reverse Polish Notation.java
@@ -1,1 +1,33 @@
-
+class Solution {
+    public int evalRPN(String[] tokens) {
+           Stack<String> s = new Stack<>();
+        
+           for(int i = 0 ; i < tokens.length;i++){ 
+               if(Character.isDigit(tokens[i].charAt(0))||tokens[i].length()>1){                   
+                   s.push(tokens[i]);
+                   continue;
+               }
+                              
+               int right = Integer.parseInt(s.pop());
+               int left = Integer.parseInt(s.pop());
+               if(tokens[i].equals("+")){
+                    int n = left+right;
+                    s.push(n+"");
+                    
+               }
+               else if(tokens[i].equals("-")){
+                    int n = left-right;
+                    s.push(n+"");                   
+               }
+               else if(tokens[i].equals("*")){
+                    int n = left*right;
+                    s.push(n+"");                   
+               }
+               else {
+                    int n = left/right;
+                    s.push(n+"");                   
+               }               
+           }
+        return Integer.parseInt(s.pop());
+    }
+}
