a2c306d67c885090adf11d5d30cec7cb62f57fec
Jiewen Ying <37558033+Cloudke@users.noreply.github.com>
2021-03-10 16:33:17 -0500

    Update 1784.java

--- 1784.java
+++ 1784.java
@@ -1,1 +1,18 @@
+// 1784. Check if Binary String Has at Most One Segment of Ones
+// Given a binary string s without leading zeros, return true
+// if s contains at most one contiguous segment of ones. Otherwise, return false.
 
+
+class Solution {
+    public boolean checkOnesSegment(String s) {
+            return s.split("0").length>1?false:true;
+    }
+}
+
+
+Java
+by @blackspinner
+
+    public boolean checkOnesSegment(String s) {
+        return !s.contains("01");
+    }
