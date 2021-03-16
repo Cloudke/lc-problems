5fccc3391c5a9818d3789ddd51aea9e5a65c29e7
Jiewen Ying <37558033+Cloudke@users.noreply.github.com>
2021-03-16 19:48:03 -0400

    Update 41.java

--- 41.java
+++ 41.java
@@ -1,1 +1,24 @@
-
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+           int first = 1;
+        
+           Arrays.sort(nums);
+        
+           for(int i = 0 ; i<nums.length;i++){
+               if(nums[i]<=0){
+                   continue;
+               }
+               else  if(nums[i] > 1 && first == 1){
+                   return 1;
+               }               
+               else if(nums[i] == first){
+                   first++;
+               }  
+               
+               else if(nums[i]>first && nums[i-1]<first &&i>1){
+                   return first;
+               }
+           }
+        return first;
+    }
+}
