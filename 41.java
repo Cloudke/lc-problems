02f8d6cd72702d68e9ca609210f6cff495df5aec
Jiewen Ying <37558033+Cloudke@users.noreply.github.com>
2021-03-26 12:37:34 -0400

    Update 41.java

--- 41.java
+++ 41.java
@@ -2,7 +2,7 @@
 
 class Solution {
     public int firstMissingPositive(int[] nums) {
-           int first = 1;
+           int first = 1;                  //start from 1 to check missing positive number
         
            Arrays.sort(nums);  //sort nums ascending
         
@@ -10,10 +10,10 @@ class Solution {
                if(nums[i]<=0){  //ignore negative numbers
                    continue;
                }
-               else  if(nums[i] > 1 && first == 1){  if first positive number is greater than 1, return 1
+               else  if(nums[i] > 1 && first == 1){  //if first positive number is greater than 1, return 1
                    return 1;
                }               
-               else if(nums[i] == first){
+               else if(nums[i] == first){           //if current number is same as first waiting number
                    first++;
                }  
                
